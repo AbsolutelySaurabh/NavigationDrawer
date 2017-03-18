2590826934354ce11a41d7c23409b2711c372a6d
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2017-03-18 13:14:43 +0530

    A NavigationDrawer with navigation views

---
+++ NavigationDrawer/app/src/main/java/com/example/absolutelysaurabh/navigationdrawer/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.absolutelysaurabh.navigationdrawer;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container,new HomeFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("Home");
+
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+               switch( item.getItemId()){
+
+                   case R.id.home_id:
+
+                       fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                       fragmentTransaction.replace(R.id.main_container,new HomeFragment());
+                       fragmentTransaction.commit();
+                       getSupportActionBar().setTitle("Home");
+                       item.setChecked(true);
+                       break;
+
+                   case R.id.id_message:
+
+                       fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                       fragmentTransaction.replace(R.id.main_container,new MessageFragment());
+                       fragmentTransaction.commit();
+                       getSupportActionBar().setTitle("Message");
+                       item.setChecked(true);
+                       break;
+
+                   case R.id.id_settings:
+
+                       fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                       fragmentTransaction.replace(R.id.main_container,new SettingsFragment());
+                       fragmentTransaction.commit();
+                       getSupportActionBar().setTitle("Settings");
+                       item.setChecked(true);
+                       break;
+               }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NavigationDrawer
+This is part of my android course which gonna teach 
+Building Navigation drawer in androdi.
+I had also added some of the elements in the NavigationView.
